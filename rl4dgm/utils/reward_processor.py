reward processor l2 computation bug fix

rl4dgm/utils/reward_processor.py
@@ -163,10 +163,13 @@ class RewardProcessor:
 
         min_distances = []
         most_similar_data_indices = []
+        human_dataset_features = torch.stack([f for f in self.human_dataset["features"]])
 
         for feature in features:
             # compute distance to all images in the human dataset
-            dists = torch.cdist(feature, features, p=2.0).mean(dim=tuple(np.arange(1, features.dim())))
+            # dists = torch.cdist(feature, features, p=2.0).mean(dim=tuple(np.arange(1, features.dim()))) # something is wrong with this
+            # TODO - hardcoded scaling factor 100
+            dists = ((human_dataset_features - feature) / 100).pow(2).sum(dim=tuple(np.arange(1, features.dim())))
             # save the smallest distance
             smallest_dist = dists.min(dim=0)
             min_distances.append(smallest_dist.values.item())