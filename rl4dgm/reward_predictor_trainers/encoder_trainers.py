06b4e588136c7e3139c7d96b383056c229d61dbb
ShangfuChen <sam145637890@gmail.com>
2024-05-31 22:55:23 +0900

    Sanity test with sd features and cross entropy loss for encoder training

--- rl4dgm/reward_predictor_trainers/encoder_trainers.py
+++ rl4dgm/reward_predictor_trainers/encoder_trainers.py
@@ -12,6 +12,169 @@ from accelerate import Accelerator
 from rl4dgm.models.mydatasets import TripletDataset, DoubleTripletDataset
 from rl4dgm.models.my_models import LinearModel
 
+
+class EntropyEncoderTrainer:
+    """
+    Class for keeping track of image encoder trained on triplet loss
+    """
+    def __init__(
+            self, 
+            config_dict: dict,
+            accelerator: Accelerator,
+            seed,
+            save_dir,
+            trainset: TripletDataset = None,
+            testset: TripletDataset = None, 
+        ):
+        """
+        Args:
+            model (nn.Module) : encoder model to train
+            trainset and testset (TripletDataset) : datasets to use for training and testing. See TripletDataset class for more detail
+            config_dict : 
+                keys: batch_size, shuffle, lr, n_epochs, triplet_margin, save_dir, save_every
+        """
+        default_config = {
+            "batch_size" : 32,
+            "shuffle" : True,
+            "lr" : 1e-6,
+            "n_epochs" : 50,
+            "save_every" : 50,
+            "input_dim" : 32768,
+            "n_hidden_layers" : 5,
+            "hidden_dims" : [22000]*6,
+            "output_dim" : 4096,
+            "name" : "entropy_encoder",
+        }
+
+        # create directory to save config and model checkpoints 
+        # assert "save_dir" in config_dict.keys(), "config_dict is missing key: save_dir"
+        os.makedirs(save_dir, exist_ok=False)
+        self.save_dir = save_dir
+            
+        # populate the config with default values if values are not provided
+        for key in default_config:
+            if key not in config_dict.keys():
+                config_dict[key] = default_config[key]
+        # hidden_dim is ListConfig type if speficied in hydra config. Convert to list so it can be dumped to json
+        config_dict["hidden_dims"] = [dim for dim in config_dict["hidden_dims"]]
+
+        print("Initializing TripletEncoderTrainer with following configs\n", config_dict)
+        with open(os.path.join(save_dir, "train_config.json"), "w") as f:
+            json.dump(config_dict, f)
+            print("saved TripletEncoderTrainer config to", os.path.join(save_dir, "train_config.json"))
+                
+        self.seed = seed
+        self.generator = torch.Generator()
+        self.generator.manual_seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+        torch.cuda.manual_seed(seed)
+
+        self.accelerator = accelerator
+        self.device = accelerator.device
+        self.model = LinearModel(
+            input_dim=config_dict["input_dim"],
+            hidden_dims=config_dict["hidden_dims"],
+            output_dim=config_dict["output_dim"],
+            device=self.device,
+        )
+        self.trainset = trainset
+        self.testset = testset
+        self.config = config_dict
+        self.name = config_dict["name"]
+
+        # Initialize dataloaders
+        self.dataloaders = {}
+        self.initialize_dataloaders(trainset, testset)
+        
+        # Initialize optimizer and loss criteria
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config["lr"])
+        self.criterion = nn.BCELoss().to(self.device)
+
+        self.n_total_epochs = 0
+        self.n_total_steps = 0
+        self.n_calls_to_train = 0 # how many times the train function has been called
+
+        self.start_time = time.time()
+
+    def initialize_dataloaders(
+            self, 
+            trainset : TripletDataset = None, 
+            testset : TripletDataset = None,
+        ): 
+        """
+        Update dataset and reinitialize dataloaders
+        Args:
+            trainset (TripletDataset)
+            testset (TripletDataset)
+        """
+        if trainset is not None:
+            self.trainset = trainset
+            self.trainloader = DataLoader(
+                trainset, 
+                batch_size=self.config["batch_size"], 
+                shuffle=self.config["shuffle"],
+                generator=self.generator,
+            )
+            self.dataloaders["train"] = self.trainloader
+        if testset is not None:
+            self.testset = testset
+            self.testloader = DataLoader(
+                testset, 
+                batch_size=self.config["batch_size"], 
+                shuffle=self.config["shuffle"],
+                generator=self.generator,
+            )
+            self.dataloaders["test"] = self.testloader
+
+    def train_model(self):
+        """
+        Trains an image encoder using cross entropy loss
+        """
+        self.n_calls_to_train += 1
+        n_steps = 0
+        for epoch in range(self.config["n_epochs"]):
+            self.n_total_epochs += 1
+            running_losses = []
+            if epoch % 100 == 0:
+                print("EntropyEncoder training epoch", epoch)
+            for step, (anchor_features, _, positive_features, negative_features) in enumerate(self.trainloader):
+                self.optimizer.zero_grad()
+                anchor_out = self.model(anchor_features)
+                positive_out = self.model(positive_features)
+                negative_out = self.model(negative_features)
+                features = torch.cat([anchor_out, positive_out, negative_out])
+                labels = torch.cat([torch.ones(anchor_out.shape[0]),
+                                   torch.ones(positive_out.shape[0]),
+                                   torch.zeros(negative_out.shape[0])]).to(self.device)
+                shuffle_indices = torch.randperm(features.shape[0])
+                features = features[shuffle_indices]
+                labels = labels[shuffle_indices]
+
+                loss = self.criterion(torch.sigmoid(features).mean(1), labels)
+                loss.backward()
+                self.optimizer.step()
+                running_losses.append(loss.item())
+                n_steps += 1
+                self.n_total_epochs += 1
+
+                self.accelerator.log({
+                    f"{self.name}_epoch" : self.n_total_epochs,
+                    f"{self.name}_step" : self.n_total_epochs,
+                    f"{self.name}_loss" : loss.item(),
+                    f"{self.name}_lr" : self.config["lr"],
+                    f"{self.name}_clock_time" : time.time() - self.start_time,
+                })
+            
+            # save checkpoint
+            if (self.n_total_epochs > 0) and (self.n_total_epochs % self.config["save_every"]) == 0:
+                model_save_path = os.path.join(self.save_dir, f"epoch{self.n_total_epochs}.pt")
+                torch.save(self.model.state_dict(), model_save_path)
+                print("EntropyEncoder model checkpoint saved to", model_save_path)
+
+
 class TripletEncoderTrainer:
     """
     Class for keeping track of image encoder trained on triplet loss
