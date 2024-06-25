3bf913c1b85e9ecb9b19537ee75160487ce0219b
ShangfuChen <sam145637890@gmail.com>
2024-06-25 17:32:38 +0900

    update train file for AI feedback

--- train_ddpo_with_human_encoder_only.py
+++ train_ddpo_with_human_encoder_only.py
@@ -140,6 +140,7 @@ def main(cfg: TrainerConfig) -> None:
     best_noise_latent = None
     best_reward = 0
     clean_data = False
+    positive_noise_latents = None
 
     n_ddpo_train_calls = 0 # number of times ddpo training loop was called
     loop = 0 # number of times the main training loop has run
@@ -148,13 +149,25 @@ def main(cfg: TrainerConfig) -> None:
         ############################################################
         # Sample from SD model
         ############################################################
-        samples, all_latents, prompts, ai_rewards = ddpo_trainer.sample(
-            logger=logger,
-            epoch=loop,
-            save_images=True,
-            img_save_dir=img_save_dir,
-            high_reward_latents=best_noise_latent)
-            # high_reward_latents=None)
+        if cfg.ddpo_conf.sample_from_best_latent:
+            print("sampling from best image latent")
+            samples, all_latents, _, _ = ddpo_trainer.sample(
+                logger=logger,
+                epoch=loop,
+                save_images=True,
+                img_save_dir=img_save_dir,
+                high_reward_latents=positive_noise_latents,
+                # high_reward_latents=best_noise_latent,
+            )
+        else:
+            print("sampling from random latent")
+            samples, all_latents, _, _ = ddpo_trainer.sample(
+                logger=logger,
+                epoch=loop,
+                save_images=True,
+                img_save_dir=img_save_dir,
+                high_reward_latents=None,
+            )
 
         sd_features = torch.flatten(all_latents[:,-1,:,:,:], start_dim=1).float()
         sd_noises = all_latents[:, 0,:,:,:].float()
@@ -197,14 +210,24 @@ def main(cfg: TrainerConfig) -> None:
         )
         human_rewards = torch.tensor(human_rewards)
         max_reward_in_batch, index = torch.max(human_rewards, 0)
-        best_index = query_indices[index]
+        best_index = query_indices[index]            
+        positive_indices = torch.nonzero(human_rewards > human_rewards.mean()).squeeze().cpu().numpy()
+        index_to_remove = np.where(positive_indices == int(index))
+        positive_indices = np.delete(positive_indices, index_to_remove)
+        positive_indices = query_indices[positive_indices]
         if max_reward_in_batch > best_reward:
-            best_image = samples[best_index].cpu().numpy().transpose(1, 2, 0) * 255
-            best_image = PIL.Image.fromarray(best_image.astype('uint8'), mode='RGB')
+            is_best_image_updated = True
             best_sample_latent = sd_features[best_index].unsqueeze(0)
             best_noise_latent = sd_noises[best_index].unsqueeze(0)
             best_reward = max_reward_in_batch
-            best_image.save("best_image.png")
+        else:
+            is_best_image_updated = False
+        
+        ### Concatenate the best latent at the end of positive latents
+        positive_noise_latents = torch.cat([
+            sd_noises[positive_indices],
+            best_noise_latent,
+        ], dim=0)
         # print("Got human rewards for query indices", query_indices)
         print("human rewards", human_rewards)
         
@@ -249,30 +272,34 @@ def main(cfg: TrainerConfig) -> None:
         with torch.no_grad():
             scores = human_dataset.human_rewards
             positive_indices = torch.nonzero(scores > scores.mean()).squeeze()
-            positive_feature = human_dataset.sd_features[positive_indices]
-            positive_sample_encodings = human_encoder_trainer.model(positive_feature)
+            positive_sample_latents = human_dataset.sd_features[positive_indices]
+            if not is_best_image_updated:
+                positive_sample_latents = torch.cat([positive_sample_latents,
+                                                     best_sample_latent])
+            positive_sample_encodings = human_encoder_trainer.model(positive_sample_latents)
 
             human_encodings = human_encoder_trainer.model(sd_features)
             best_sample_encoding = human_encoder_trainer.model(best_sample_latent)
         print("\nComputing final rewards")
 
         ### Similarity to all positive samples ###
-        # expand_dim = (positive_sample_encodings.shape[0], human_encodings.shape[0], human_encodings.shape[1])
-        # positive_sample_encodings = positive_sample_encodings.unsqueeze(1).expand(expand_dim)
-        # human_encodings = human_encodings.unsqueeze(0).expand(expand_dim)
-        # final_rewards = torch.nn.functional.cosine_similarity(human_encodings, positive_sample_encodings, dim=2).mean(0)
+        if cfg.ddpo_conf.reward_mode == "similarity-to-all-positive":
+            expand_dim = (positive_sample_encodings.shape[0], human_encodings.shape[0], human_encodings.shape[1])
+            positive_sample_encodings = positive_sample_encodings.unsqueeze(1).expand(expand_dim)
+            human_encodings = human_encodings.unsqueeze(0).expand(expand_dim)
+            final_rewards = torch.nn.functional.cosine_similarity(human_encodings, positive_sample_encodings, dim=2).mean(0)
+            final_rewards = (final_rewards+1)/2
+        elif cfg.ddpo_conf.reward_mode == "similarity-to-best-sample":
+        ### Similarity to the best sample ### (for similarity-based reward)
+            final_rewards = torch.nn.functional.cosine_similarity(human_encodings, best_sample_encoding.expand(human_encodings.shape))
+            final_rewards = (final_rewards+1)/2
         
         if clean_data:
             human_dataset.clear_data()
             clean_data = False
-        ### Similarity to the best sample ### (for similarity-based reward)
-        final_rewards = torch.nn.functional.cosine_similarity(human_encodings, best_sample_encoding.expand(human_encodings.shape))
-        final_rewards = (final_rewards+1)/2
-        # final_rewards = torch.softmax(final_rewards, dim=0)
 
         ### Ground truth human reward ### (for query everything)
         # final_rewards = human_rewards - 19.2
-        # final_rewards = torch.softmax(final_rewards, dim=0)
 
         ai_indices = np.setdiff1d(np.arange(sd_features.shape[0]), np.array(query_indices)) # feature indices where AI reward is used
 
